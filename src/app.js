f8b345c69747feedc785cb8746aeeebe0fd8a02d
ShubhamJPakhale <pakhaleshubham42@gmail.com>
2025-07-10 18:27:24 +0530

    fixed cors error to support api call

--- src/app.js
+++ src/app.js
@@ -2,14 +2,22 @@ const express = require("express");
 const { connecttoDB } = require("./config/database");
 require("dotenv").config();
 const cookieparser = require("cookie-parser");
+const cors = require("cors");
 const authRouter = require("./routes/auth");
 const profileRoute= require("./routes/profile");
 const connectionRequestRoute = require("./routes/connectionRequest");
 const userRoute = require("./routes/user");
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors(
+  {
+    origin: "http://localhost:5173",
+    credentials: true,
+  }
+));
 app.use(express.json());
 app.use(cookieparser());
 
