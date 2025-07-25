fe27e61ef1fb764f2d6fe3f8bbe6892b262fd09e
ShubhamJPakhale <pakhaleshubham42@gmail.com>
2025-07-25 12:40:20 +0530

    mongodb changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connecttoDB = async () => {
-  await mongoose.connect(process.env.DB_CONNECTION_SECRET,{ useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 };
 
 module.exports = { connecttoDB };
